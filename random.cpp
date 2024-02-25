ff52397f4d339859664e640c75def2f79d66f5de
Danil <dsb35@tpu.ru>
2024-02-25 20:51:17 +0700

    1

---
+++ random.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+struct DataBase {
+	vector <string> NameStudent;
+	int age;
+	int grade;
+	vector <string> lessson;
+};
+
+
+int main(){
+	setlocale(LC_ALL, "rus");
+
+
+
+	return 0;
+}
